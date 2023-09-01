7679038fe3d37d75901fad89d84c0854cd1e5f58
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-09-01 20:12:25 -0300

    Testing Componente App

--- src/App.js
+++ src/App.js
@@ -40,7 +40,9 @@ function App() {
 
   return (
     <Fragment>
-      <h1>Administrador de Pacientes</h1>
+      <h1
+        data-testid="nombre-app"
+      >Administrador de Pacientes</h1>
 
       <div className="container">
         <div className="row">
--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,1 +1,15 @@
-test("La aplicación funciona correctamente", () => {});
+import React from "react";
+import App from "../App";
+import { render, screen, clean, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+
+test("<App/ >La aplicación funciona correctamente", () => {
+    // const wrapper = render(<App />);
+    // wrapper.debug();
+
+    render(<App />)
+    expect(screen.getByText("Administrador de Pacientes")).toBeInTheDocument();
+    expect(screen.getByTestId("nombre-app").textContent).toBe("Administrador de Pacientes");
+    expect(screen.getByTestId("nombre-app").tagName).toBe("H1");
+});
