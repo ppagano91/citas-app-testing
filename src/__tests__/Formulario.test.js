fd9788bd5a532fd1740d34b20970ff37f22a742c
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-09-01 19:24:25 -0300

    Probando Funciones, Submits de Formulario y Validación

--- src/__tests__/Formulario.test.js
+++ src/__tests__/Formulario.test.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Formulario from "../components/Formulario";
-import { render, screen } from "@testing-library/react";
+import { render, screen, clean, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+
+const crearCita = jest.fn();
+
+// cleanup: https://testing-library.com/docs/react-testing-library/api#cleanup
+// Ya no es necesario limpiar el DOM después de cada prueba, ya que se hace automáticamente.
+// afterEach(clean());
 
 test("<Formulario/> Componente funciona correctamente", () => {
     // renderizar el componente
@@ -17,7 +23,7 @@ test("<Formulario/> Componente funciona correctamente", () => {
     // expect(wrapper.getByText("Crear Cita")).toBeInTheDocument();
 
     // OPCION 3
-    render(<Formulario />);
+    render(<Formulario crearCita={crearCita}/>);
     const titulo = screen.getByTestId("titulo");
     expect(screen.getByText("Crear Cita")).toBeInTheDocument();
     expect(titulo.tagName).toBe("H2");
@@ -30,14 +36,32 @@ test("<Formulario/> Componente funciona correctamente", () => {
     // expect(wrapper.getByTestId("btn-submit").textContent).toBe("Agregar Cita");
     expect(btnSubmit.tagName).toBe("BUTTON");
     expect(btnSubmit.textContent).toBe("Agregar Cita");
-    expect(btnSubmit.textContent).not.toBe("Agregar Nueva Cita");
-
+    expect(btnSubmit.textContent).not.toBe("Agregar Nueva Cita");    
     
-    // // verificar por tipo de elemento
-    // expect(wrapper.getByTestId("btn-submit").tagName).toBe("BUTTON");
     
     // // verificar por clase de elemento
     // expect(wrapper.getByTestId("btn-submit").className).toBe(
     //     "btn btn-block btn-primary"
     // );
+});
+
+test("<Formulario/> Validación de formulario", () => {
+    render(<Formulario crearCita={crearCita}/>);
+
+    // Click en el botón de submit
+    const btnSubmit = screen.getByTestId("btn-submit");
+    fireEvent.click(btnSubmit);
+
+    // Revisar por la alerta
+    const alerta = screen.getByTestId("alerta");
+    expect(alerta).toBeInTheDocument();
+    expect(alerta.textContent).toBe("Todos los campos son obligatorios");
+    expect(alerta.tagName).toBe("P");
+    expect(alerta.tagName).not.toBe("BUTTON");
+    // expect(btnSubmit.tagName).toBe("BUTTON");
+    // expect(btnSubmit.textContent).toBe("Agregar Cita");
+    // expect(btnSubmit.textContent).not.toBe("Agregar Nueva Cita");    
+    // expect(crearCita).toHaveBeenCalledTimes(1);
+    // expect(crearCita).toHaveBeenCalled();
+    // expect(crearCita).toHaveBeenCalledWith("Hola");
 });
